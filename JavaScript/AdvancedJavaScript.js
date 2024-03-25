b8e758e616a6e12aa46c774ab4fe45ea95679ad1
Dpaace <dipeshnepali767@gmail.com>
2024-03-25 17:55:40 +0545

    Updates on the course

--- JavaScript/AdvancedJavaScript.js
+++ JavaScript/AdvancedJavaScript.js
@@ -29,29 +29,69 @@
 // Advanced methods that can be used on arrays 
 
 // forEach() method
-const fruits = ["kiwi", "mango", "pear", "apple", "orange"];
+// const fruits = ["kiwi", "mango", "pear", "apple", "orange"];
 
-function appendIndex(fruit, index) {
-    console.log(`${index}.${fruit}`);
+// function appendIndex(fruit, index) {
+//     console.log(`${index}.${fruit}`);
+// }
+
+// fruits.forEach(appendIndex);
+
+
+
+// // filter() method
+// const nums = [10, 20, 30, 40, 50, 60];
+
+// var val = nums.filter(function (num) {
+//     return num > 30;
+// })
+
+// console.log(val);
+
+// // map() method
+
+// var div = nums.map( function(num) {
+//     return num/10;
+// })
+
+// console.log(div);
+
+// Spread and Rest Operator
+
+// const fruits = ['apple', 'pear', 'plum']
+// const berries = ['blueberry', 'strawberry']
+// const fruitsAndBerries = [...fruits, ...berries] // concatenate
+// console.log(fruitsAndBerries);
+
+// const flying = { wings: 2 }
+// const car = { wheels: 4 }
+// const flyingCar = {...flying, ...car}
+// console.log(flyingCar)
+
+// let veggies = ['onion', 'parsley'];
+// veggies = [...veggies, 'carrot', 'beetroot'];
+// console.log(veggies);
+
+// const greeting = "Hello";
+// const arrayOfChars = [...greeting];
+// console.log(arrayOfChars);
+
+// const meal = ["soup", "steak", "ice cream"]
+// let [starter] = meal;
+// console.log(starter);
+
+// let obj = {
+//     key: 1,
+//     value: 4
+// };
+
+// let output = { ...obj };
+// output.value -= obj.key;
+
+// console.log(output.value);
+
+function count(...basket) {
+    console.log(basket.length)
 }
 
-fruits.forEach(appendIndex);
-
-
-
-// filter() method
-const nums = [10, 20, 30, 40, 50, 60];
-
-var val = nums.filter(function (num) {
-    return num > 30;
-})
-
-console.log(val);
-
-// map() method
-
-var div = nums.map( function(num) {
-    return num/10;
-})
-
-console.log(div);
+count(10, 9, 8, 7, 6);
