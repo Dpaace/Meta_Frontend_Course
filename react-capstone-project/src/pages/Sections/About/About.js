637af4be3b9e98f145496d6af24bf431f12fe895
Dpaace <dipeshnepali767@gmail.com>
2024-05-29 14:28:58 +0545

    About Little Lemon Section Design Completed

--- react-capstone-project/src/pages/Sections/About/About.js
+++ react-capstone-project/src/pages/Sections/About/About.js
@@ -1,12 +1,46 @@
 import React from "react";
 import "./About.css";
+import Image from "../../../assets/restauranfood.jpg";
+import ImageRestaurant from "../../../assets/restaurant.jpg";
 
 const About = () => {
     return (
-        <>
-            <h1> About </h1>
-        </>
-    )
-}
+        <div className="bg-white">
+            <div className="about">
+                <div>
+                    <h1 className="about-title">Little Lemon</h1>
+                    <h2 className="about-subtitle">Nepal</h2>
+                    <p className="about-description">
+                        Nestled in the heart of bustling Chicago, Little Lemon is where modern
+                        flair meets cozy nostalgia. Our diverse, artisanal menu, featuring
+                        delectable bruschettas, succulent burgers, and refreshing Greek
+                        salads, is a testament to our belief that food is an art. Crafted by
+                        expert chefs with locally-sourced ingredients, our dishes not only
+                        delight the palate but nourish the soul. Whether you're seeking a
+                        romantic dinner, a family meal, or a relaxed atmosphere with friends,
+                        Little Lemon is your ideal culinary destination. Find us in the heart
+                        of Chicago for an unforgettable experience.
+                    </p>
+                </div>
+
+                <img
+                    className="about-image-1"
+                    src={ImageRestaurant}
+                    alt="Restarurant food"
+                    height={200}
+                    width={200}
+                />
+                <img
+                    className="about-image-2"
+                    src={Image}
+                    alt="Restarurant food"
+                    height={200}
+                    width={200}
+                />
+            </div>
+        </div>
+    );
+};
+
 
 export default About
