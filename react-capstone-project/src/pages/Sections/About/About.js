0129c47bd28f17bdfb0e393f3911196eab5200b7
Dpaace <dipeshnepali767@gmail.com>
2024-05-29 14:32:15 +0545

    Updates in the About Section

--- react-capstone-project/src/pages/Sections/About/About.js
+++ react-capstone-project/src/pages/Sections/About/About.js
@@ -11,29 +11,25 @@ const About = () => {
                     <h1 className="about-title">Little Lemon</h1>
                     <h2 className="about-subtitle">Nepal</h2>
                     <p className="about-description">
-                        Nestled in the heart of bustling Chicago, Little Lemon is where modern
-                        flair meets cozy nostalgia. Our diverse, artisanal menu, featuring
-                        delectable bruschettas, succulent burgers, and refreshing Greek
-                        salads, is a testament to our belief that food is an art. Crafted by
-                        expert chefs with locally-sourced ingredients, our dishes not only
-                        delight the palate but nourish the soul. Whether you're seeking a
-                        romantic dinner, a family meal, or a relaxed atmosphere with friends,
-                        Little Lemon is your ideal culinary destination. Find us in the heart
-                        of Chicago for an unforgettable experience.
+                        Welcome to Little Lemon, a charming and vibrant restaurant located in the heart of Nepal.
+                        We pride ourselves on offering a delightful culinary experience, combining traditional Nepalese flavors with a modern twist.
+                        Our menu is crafted from the freshest ingredients, ensuring that each dish is bursting with flavor and authenticity.
+                        Whether you're here for a casual meal or a special occasion, Little Lemon promises a warm atmosphere and exceptional service.
+                        Come and savor the taste of Nepal at Little Lemon!
                     </p>
                 </div>
 
                 <img
                     className="about-image-1"
                     src={ImageRestaurant}
-                    alt="Restarurant food"
+                    alt="Restaurant food"
                     height={200}
                     width={200}
                 />
                 <img
                     className="about-image-2"
                     src={Image}
-                    alt="Restarurant food"
+                    alt="Restaurant food"
                     height={200}
                     width={200}
                 />
@@ -41,6 +37,7 @@ const About = () => {
         </div>
     );
 };
+
 
 
 export default About
